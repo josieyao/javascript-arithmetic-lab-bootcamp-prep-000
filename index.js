b4b06438b3649c82f4c5ae0f1ee82e292b750056
Josephine Yao <yao.josephine@yahoo.com>
2019-01-08 22:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,15 +54,36 @@ function dec(a){
   return a -= 1;
 }
 
-function makeInt(a){
-  var n = parseInt(a);
-  return n;
-}
+/*describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})*/
 
 function makeInt(a){
   var n = parseInt(a, 10);
   return n;
 }
+
+
 
 function preserveDecimal(a){
   var n = parseFloat(a, 10);
