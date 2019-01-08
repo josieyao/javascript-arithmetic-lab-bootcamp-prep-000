964f0f9f9a2c42ffa50bedbaeb32f7d47f2dc04a
Josephine Yao <yao.josephine@yahoo.com>
2019-01-08 21:59:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+/*var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+it('add(a, b) adds two numbers and returns the result', function() {
+  expect(add(a, b)).toEqual(a + b)
+})*/
+
 function add(a, b){
   return a + b;
 }
+
+
 
 function subtract(a,b){
   var subtracted = (a - b);
